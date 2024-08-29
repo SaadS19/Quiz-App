9f6e30160be4977aa35d2d7833f404e90d79b92f
SaadS19 <121642559+SaadS19@users.noreply.github.com>
2024-08-29 10:58:26 +0500

    Countdown done

--- src/store/countdown/mutations.js
+++ src/store/countdown/mutations.js
@@ -1,18 +1,23 @@
 export default {
   startTime(state) {
     let timer = state.time;
-    console.log(timer);
+    if (timer > 0) {
+      let minutes = Math.floor(timer / 60);
+      let seconds = timer % 60;
 
-    let minutes = Math.floor(timer / 60);
-    let seconds = timer % 60;
+      state.minutes = minutes < 10 ? "0" + minutes : minutes;
+      state.seconds = seconds < 10 ? "0" + seconds : seconds;
 
-    state.minutes = minutes < 10 ? "0" + minutes : minutes;
-    state.seconds = seconds < 10 ? "0" + seconds : seconds;
-
-    state.time--;
+      state.time--;
+    }
   },
-  stopTimer(state) {
-    state.time = 0;
-    state.currQuestion = state.questions.length + state.questions.length;
+
+  setIntervalId(state, id) {
+    state.intervalId = id;
+  },
+
+  clearInterval(state) {
+    clearInterval(state.intervalId);
+    state.intervalId = null;
   },
 };
